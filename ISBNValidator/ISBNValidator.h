0ac969f3c03b6ab6fd0b381838f9cea988a161d5
Tim Duckett <tim.duckett@oetkerdigital.com>
2017-10-28 13:28:23 +0200

    Initial Commit

---
+++ ISBNValidator/ISBNValidator.h
@@ -0,0 +1,19 @@
+//
+//  ISBNValidator.h
+//  ISBNValidator
+//
+//  Created by Tim Ducket on 28.10.17.
+//  Copyright © 2017 Tim Duckett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISBNValidator.
+FOUNDATION_EXPORT double ISBNValidatorVersionNumber;
+
+//! Project version string for ISBNValidator.
+FOUNDATION_EXPORT const unsigned char ISBNValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISBNValidator/PublicHeader.h>
+
+
